729bf93b7885192910635eca47ae94f9df554e67
eazytrainingfr <66882176+eazytrainingfr@users.noreply.github.com>
2022-11-02 17:47:42 +0100

    change AMI ID

--- tp-2/ec2.tf
+++ tp-2/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-0057d8e6fb0692b80"
+  ami           = "ami-00e87074e52e6c9f9"
   instance_type = "t2.micro"
   key_name      = "devops-dirane"
   tags = {
