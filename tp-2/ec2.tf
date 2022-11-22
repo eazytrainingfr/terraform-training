65d8c285a4f1c0693f3c20d52da1a5de2755591f
Dirane TAFEN <diranetafen@yahoo.com>
2022-11-22 08:23:11 +0100

    update image

--- tp-2/ec2.tf
+++ tp-2/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-00e87074e52e6c9f9"
+  ami           = "ami-012cc038cc685a0d7"
   instance_type = "t2.micro"
   key_name      = "devops-dirane"
   tags = {
