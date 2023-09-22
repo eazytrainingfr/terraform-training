9caa7d704b11d2329eadeaf2a442b881e09dfcf7
kamatador9 <kamsukom9@gmail.com>
2023-09-22 11:20:06 +0100

    update syntax

--- TP sur AWS/tp-5/ec2.tf
+++ TP sur AWS/tp-5/ec2.tf
@@ -86,7 +86,7 @@ resource "aws_security_group" "allow_ssh_http_https" {
 
 resource "aws_eip" "lb" {
   instance = aws_instance.myec2.id
-  vpc      = true
+  domain   = "vpc"
   provisioner "local-exec" {
     command = "echo PUBLIC IP: ${aws_eip.lb.public_ip} ; ID: ${aws_instance.myec2.id} ; AZ: ${aws_instance.myec2.availability_zone}; >> infos_ec2.txt"
   }
